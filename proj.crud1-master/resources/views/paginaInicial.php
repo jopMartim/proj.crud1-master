da5bee524f45a8168d0d13830defdb02873f7ceb
RafaelFurtadoMartins <rafael.martins2231@gmail.com>
2020-09-10 16:36:35 -0300

    Initial commit

---
+++ proj.crud1-master/resources/views/paginaInicial.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+</head>
+
+<body>
+    <ion-card-title>Cadastro De Contatos</ion-card-title>
+    <html>
+
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <form action="" method="POST">
+
+        <p>
+            Nome:<br />
+            <input type="text" size="30" name="nome">
+        </p>
+
+        <p>
+            E-mail:<br />
+            <input type="text" size="30" name="replyto">
+        </p>
+
+        <p>
+            Telefone:<br />
+            <input type="text" size="30" name="Comentarios">
+        </p>
+
+        <p>
+            <input type="submit" name="BTEnvia" value="Salvar">
+    </form>
+    <p>
+            <input type="submit" name="Autoresprojeto" value="Quem fez o projeto!">
+    </html>
+</body>
+
+</html>
